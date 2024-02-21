44b9947ae26cdcac2c140d7ac5eadc62ee4b8513
Vitek169 <sambo911@mail.ru>
2024-02-21 16:21:49 +0300

    добавляем path cats slug и id

--- sitewoman/women/urls.py
+++ sitewoman/women/urls.py
@@ -6,6 +6,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index), # изменяем главную страницу http://127.0.0.1:8000 с выводом статьи 'Страница приложения women.' из views
-    path('cats/', views.categories), #добавляет суффикс women к доменному имени http://127.0.0.1:8000/categories
+    path('cats/<int:cat_id>/', views.categories), #добавляет суффикс women к доменному имени http://127.0.0.1:8000/categories
+    path('cats/<slug:cat_slug>/', views.categories_by_slug)
 
 ]
+
+
+
+
--- sitewoman/women/views.py
+++ sitewoman/women/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(requset): # HttpRequest
+def index(request): # HttpRequest
     return HttpResponse('Страница приложения women.') # Возврат страницы с надписю про women
 
-def categories(request):
-    return HttpResponse('<h1>Статьи по категориям </h1>') # Возврат страницы с надписю про categories
+def categories(request, cat_id):
+    return HttpResponse(f'<h1>Статьи по категориям </h1><p>id: {cat_id}</p>') # Возврат страницы с надписю про categories
+
+def categories_by_slug(request, cat_slug): # добавляем возможность вводить не только int, но и любые символы
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
+
+
