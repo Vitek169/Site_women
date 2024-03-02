ad838a5b7ac68f83fa163332765ee63a110a9c5c
Vitek169 <sambo911@mail.ru>
2024-03-02 18:33:30 +0300

    Добавление тегов for и if в HTML странички

--- sitewoman/women/views.py
+++ sitewoman/women/views.py
@@ -3,13 +3,25 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
-def index(request): # HttpRequest
-    # t = render_to_string('women/index.html')
-    # return HttpResponse(t)
-    return render(request, 'women/index.html')
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+]
+
+
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
+    }
+    return render(request, 'women/index.html', context=data)
 def about(request):
-    return render(request, 'women/about.html')
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Статьи по категориям </h1><p>id: {cat_id}</p>') # Возврат страницы с надписю про categories
