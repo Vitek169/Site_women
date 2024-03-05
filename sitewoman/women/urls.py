367bddbd9b903aa3748eeb1c690300235ddeae9d
Vitek169 <sambo911@mail.ru>
2024-03-05 20:17:14 +0300

    Небольшие изменения

--- sitewoman/women/urls.py
+++ sitewoman/women/urls.py
@@ -9,9 +9,10 @@ register_converter(converters.FourDigitYearConverter, "year4")
 urlpatterns = [
     path('', views.index, name='home'), # изменяем главную страницу http://127.0.0.1:8000 с выводом статьи 'Страница приложения women.' из views
     path('about/', views.about, name='about'),
-    path('cats/<int:cat_id>/', views.categories, name='cats-id'), #добавляет суффикс women к доменному имени http://127.0.0.1:8000/categories
-    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
-    path('archive/<year4:year>/', views.archive, name='archive'),
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 
 ]
 
--- sitewoman/women/views.py
+++ sitewoman/women/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
@@ -21,20 +25,21 @@ def index(request):
     }
     return render(request, 'women/index.html', context=data)
 def about(request):
-    return render(request, 'women/about.html', {'title': 'О сайте'})
+    return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
-def categories(request, cat_id):
-    return HttpResponse(f'<h1>Статьи по категориям </h1><p>id: {cat_id}</p>') # Возврат страницы с надписю про categories
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с ig = {post_id}')
 
-def categories_by_slug(request, cat_slug): # добавляем возможность вводить не только int, но и любые символы
-    return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
-def archive(request, year):
-    if year > 2024:
-        uri = reverse('cats', args=('music',))
-        return redirect(uri)
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
 
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
