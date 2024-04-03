5efda7c9f5674758ebac1cbdecc4b85840595037
Vitek169 <sambo911@mail.ru>
2024-04-03 15:54:36 +0300

    Создание миграции

--- sitewoman/women/models.py
+++ sitewoman/women/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Women(models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    content = models.TextField(blank=True) # Можно пропустить информацию, поставив True
+    time_create = models.DateTimeField(auto_now_add=True) # Заполнение впемени атоматически при публикации
+    time_update = models.DateTimeField(auto_now=True) # Изменяется время и дата при обнафылении инфы
+    is_published = models.BooleanField(default=True) # Булево значение. По дефолту True
 
 
 
